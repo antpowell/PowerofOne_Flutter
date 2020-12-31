721f1b5d31981811c5b87d48c1d6b51c0733f618
Joshua Holly <joshuadholly@outlook.com>
2020-12-31 10:54:41 -0600

    Merge branch 'firebase_feature' of https://github.com/antpowell/PowerofOne_Flutter into Button_implementation
    # Conflicts:
    #	lib/Views/FeedBack/FeedBack.dart
    #	lib/Views/TermsAndConditions/TermsAndConditions.dart

--- lib/Views/FeedBack/FeedBack.dart
+++ lib/Views/FeedBack/FeedBack.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:power_one/Views/Buttons/PO1Button.dart';
+import 'package:power_one/Objects/PO1Score.dart';
+import 'package:power_one/models/PO1User.dart';
 
 class FeedBack extends StatelessWidget {
   static final String id = 'feedback_screen';
   const FeedBack({Key key, this.title}) : super(key: key);
-  final title;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
+    final PO1User _user = PO1User();
+
     Color color = Theme.of(context).primaryColor;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black12,
         title: Text(
-          'Power of 1 Score Feed Back',
+          'Power of 1 Score FeedBack',
           style: TextStyle(
             fontSize: 30,
           ),
@@ -26,56 +30,229 @@ class FeedBack extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-
               Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text("\nShooting Percentage\n",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
-                      )),
-                  Text("Field Goal Percentage: \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                  Text("    Free Throw Percentage: \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
+                  Text("\n"),
+                  Row(
+                    children: [
+                      Text("Field Goal Percentage:",
+                          style: TextStyle(
+                            color: Colors.white,fontSize: 20,
+                            decoration: TextDecoration.underline,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(_user.score.getAverages()['FG'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                       child: Text(
+                            _user.score.feedback(_user.score.getAverages())['FG'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ],
+                  ),
+
+                  Row(
+
+                    children: [
+                      Text("\n"),
+                      Text("Free Throw Percentage:",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            decoration: TextDecoration.underline,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.getAverages()['1PT'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.feedback(_user.score.getAverages())['1PT'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ],
+                  ),
                 ],
               ),
 
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Hustle Points\n",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
-                      )),
-                  Text("Offensive Rebounds: \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                  Text("Blocks: \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                  Text("Steals: \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                  Text("Assists: \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
-                  Text("Turnovers: \n",
-                      style: TextStyle(
-                        color: Colors.white,
-                      )),
+                  Text("\n"),
+                  Row(
+                    children: [
+                      Text("Offensive Rebounds:",
+                          style: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.underline,
+                            fontSize: 20,
+                          )),
+                    ],
+                  ),
+                Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: EdgeInsets.all(10.0),
+                      child: Text( _user.score.hustlePointsMap['ORB'].pos.toString(),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: EdgeInsets.all(10.0),
+                      child: Text(
+                          _user.score.hustlepointsfeedback(_user.score.hustlePointsMap)['ORB'].toString(),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("\n"),
+                    Text("Blocks:",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          decoration: TextDecoration.underline,
+                        )),
+                  ],
+                ),
+                  Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlePointsMap['BLKS'].pos.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlepointsfeedback(_user.score.hustlePointsMap)['BLKS'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Steals:",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize:20,
+                            decoration: TextDecoration.underline,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlePointsMap['STLS'].pos.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlepointsfeedback(_user.score.hustlePointsMap)['STLS'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Assists:",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            decoration: TextDecoration.underline,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlePointsMap['ASST'].pos.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlepointsfeedback(_user.score.hustlePointsMap)['ASST'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Turnovers:",
+                          style: TextStyle(
+                            color: Colors.white,
+                            decoration: TextDecoration.underline,
+                            fontSize: 20,
+                          )),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlePointsMap['TO'].pos.toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                      Container(
+                        alignment: Alignment.topLeft,
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                            _user.score.hustlepointsfeedback(_user.score.hustlePointsMap)['TO'].toString(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                      ),
+                    ],
+
+                  ),
+
                 ],
               ),
             ],
