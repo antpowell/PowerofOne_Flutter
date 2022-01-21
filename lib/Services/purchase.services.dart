83b50fb2b2d850520f5e41d4e0328e0e6dd38b02
Powell, Anthony <anpowe@microsoft.com, powell.anthony.e@gmail.com>
2022-01-20 18:28:30 -0600

    halfway through video

---
+++ lib/Services/purchase.services.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/services.dart';
+
+// video: https://www.youtube.com/watch?v=h-jOMh2KXTA
+
+class Offers {}
+
+// class PurchaseService {
+//   static const APIKEY = 'get from env variables';
+//   static Future init() async {}
+//   static Future<List<Offers>> featchOffers() async {
+//     try {
+//       final current = 'offers';
+//       return current == null ? [] : [current];
+//     } on PlatformException catch (e) {
+//       print(e);
+//       return [];
+//     }
+//   }
+// }
