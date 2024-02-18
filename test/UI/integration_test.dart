def10e62527ca2f97202ef068c2ee1cb8cab69b4
Powell, Anthony <anpowe@microsoft.com, powell.anthony.e@gmail.com>
2024-02-18 02:02:40 -0600

    add ui integration testing framework

---
+++ test/UI/integration_test.dart
@@ -0,0 +1,45 @@
+import 'package:power_one/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
+
+void main() {
+  patrolTest(
+    'counter state is the same after going to home and going back',
+    ($) async {
+      await $.pumpWidgetAndSettle(Power1());
+
+      await $(FloatingActionButton).tap();
+      expect($(#counterText).text, '1');
+
+      await $.native.pressHome();
+      await $.native.pressDoubleRecentApps();
+
+      expect($(#counterText).text, '1');
+      await $(FloatingActionButton).tap();
+      expect($(#counterText).text, '2');
+
+      await $.native.openNotifications();
+      await $.native.pressBack();
+    },
+  );
+  patrolTest(
+    'log user in',
+    ($) async {
+      await $.pumpWidgetAndSettle(Power1());
+
+      await $(FloatingActionButton).tap();
+      expect($(#counterText).text, '1');
+
+      await $.native.pressHome();
+      await $.native.pressDoubleRecentApps();
+
+      expect($(#counterText).text, '1');
+      await $(FloatingActionButton).tap();
+      expect($(#counterText).text, '2');
+
+      await $.native.openNotifications();
+      await $.native.pressBack();
+    },
+  );
+}
